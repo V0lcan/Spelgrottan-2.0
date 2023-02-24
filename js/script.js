8694bc05b922b404645f28c3727a72e09b51aa6b
V0lcan <albinsand@hotmail.se>
2023-02-24 15:43:04 +0100

    Garbage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+// Navbar fade effect
 // Set the initial opacity of the navbar to 0
 $('#navbar').css({
     opacity: 0,
-    transition: 'opacity 0.6s ease-in-out'
+    transition: 'opacity 0.3s ease-in-out'
   });
 
 // Add an event listener to the window object for the scroll event
@@ -27,3 +28,29 @@ $(window).on('scroll', function() {
   // Set the opacity of the navbar
   $('#navbar').css('opacity', opacity);
 });
+
+// Header fade effect
+$(window).scroll(function() {
+    $('.wrapper').each(function() {
+      // Get the position of the element on the page
+      var imagePos = $(this).offset().top;
+  
+      // Get the height of the viewport
+      var windowHeight = $(window).height();
+  
+      // Get the current position of the top of the viewport
+      var topOfWindow = $(window).scrollTop();
+  
+      // Define the distance from the top of the viewport at which the fade-in effect should trigger
+      var triggerPoint = 150;
+  
+      // If the top of the element is within the distance from the top of the viewport to trigger the effect
+      if (imagePos < topOfWindow + windowHeight - triggerPoint) {
+        $(this).fadeIn(1000);
+      } else {
+        $(this).fadeOut(1000);
+      }
+    });
+  });
+  
+  
